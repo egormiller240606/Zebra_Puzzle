43581cfe42c255da342d66572ec535d5f11366c2
egormiller240606 <egormiller240606@gmail.com>
2026-09-03 19:06:14 +0300

    Update __init__.py

--- events/__init__.py
+++ events/__init__.py
@@ -1,4 +1,3 @@
-# Events module
 from .base import Event, EVENT_PRIORITY_FINISH_TRIP, EVENT_PRIORITY_EXCHANGE, EVENT_PRIORITY_START_TRIP
 from .trip import StartTripEvent, FinishTripEvent
 from .exchange import ChangeHouseEvent, ChangePetEvent
